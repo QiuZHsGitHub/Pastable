22c8a78a7ca421a9b1280ebe1b6ff04842905285
qzh <h593526370@gmail.com>
2025-12-29 23:01:57 +0800

    Initial Commit

---
+++ Pastable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pastable
+//
+//  Created by qzh on 2025/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pastable/PastableApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastableApp.swift
+//  Pastable
+//
+//  Created by qzh on 2025/12/29.
+//
+
+import SwiftUI
+
+@main
+struct PastableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
